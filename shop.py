867375eb3840d922e12d2e31b76fc2f15e4f59d2
President Carter <olabodebalogun80@gmail.com>
2024-03-04 10:48:53 +0100

    04 March 2024

---
+++ shop.py
@@ -0,0 +1,20 @@
+class Shop:
+    def __init__(self, name, brand, quantity, price):
+        self.name = name
+        self.brand = brand
+        self.quantity = quantity
+        self.price = price
+        
+        
+    def sell(self):
+        print(f"You've purchased {self.quantity} {self.name} for {self.price}")
+    
+    def buy(self):
+        pass
+    
+# class Dealer(Shop):
+    
+    
+#     if
+
+
---
+++ test2.py
@@ -0,0 +1,4 @@
+from shop import Shop
+
+grocery = Shop("Perishables", "Dangote", 50, 300.00)
+grocery.sell()
